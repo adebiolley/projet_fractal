d144afedd355b064c2a362b3cff6c828b0736ebf
adebiolley <37015632+adebiolley@users.noreply.github.com>
2018-05-02 17:36:21 +0200

    Update header.c

--- header.c
+++ header.c
@@ -1,1 +1,12 @@
 
+#include header.h
+
+void sbuf_init(sbuf_t *sp, int n)
+{
+    sp->buf = calloc(n, sizeof(int));
+    sp->n = n;                       /* Buffer content les entiers */
+    sp->front = sp->rear = 0;        /* Buffer vide si front == rear */
+    sem_init(&sp->mutex, 0, 1);      /* Exclusion mutuelle */
+    sem_init(&sp->slots, 0, n);      /* Au début, n slots vides */
+    sem_init(&sp->items, 0, 0);      /* Au début, rien à consommer */
+}
